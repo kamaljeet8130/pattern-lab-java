61ec6500e98f71733f614ffe2d5ac949fb6e0cd1
kamaljeet8130 <kamaljeet8130@gmail.com>
2025-11-21 15:09:27 +0530

    Patter 1

--- src/patternlabjava/Pattern1.java
+++ src/patternlabjava/Pattern1.java
@@ -1,5 +1,24 @@
 package patternlabjava;
 
-public class Pattern1 {
+import util.InputUtil;
+/*
+Enter::
+4
+ *  *  *  *
+ *  *  *  *
+ *  *  *  *
+ *  *  *  *
 
+ */
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        int n = InputUtil.nextInt();
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<n;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
 }
