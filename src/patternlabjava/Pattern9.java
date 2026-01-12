f8a23a5280cba20e6efc1b242c87a459a0dbb944
kamaljeet8130 <kamaljeet8130@gmail.com>
2026-01-12 18:20:30 +0530

    Pattern 9

--- src/patternlabjava/Pattern9.java
+++ src/patternlabjava/Pattern9.java
@@ -5,34 +5,21 @@ import util.InputUtil;
 public class Pattern9 {
     public static void main(String[] args) {
         int n = InputUtil.nextInt();
-        for(int i = 1;i<n;i++){
-            for(int j =1;j<n;j++){
-                if(i+j<n){
-                    System.out.print( "  ");
-                }
-                else{
-                    System.out.print( " * ");
-
-                }
-
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(j * i + " ");
             }
             System.out.println();
         }
+
     }
 }
-
 /*
-
 Enter::
-10
-                 *
-               *  *
-             *  *  *
-           *  *  *  *
-         *  *  *  *  *
-       *  *  *  *  *  *
-     *  *  *  *  *  *  *
-   *  *  *  *  *  *  *  *
- *  *  *  *  *  *  *  *  *
-
+5
+1 2 3 4 5
+2 4 6 8 10
+3 6 9 12 15
+4 8 12 16 20
+5 10 15 20 25
 * */
