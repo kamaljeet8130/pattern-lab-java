986210f262499d5fd9251a57ab8eaa6c344a9a06
kamaljeet8130 <kamaljeet8130@gmail.com>
2026-01-24 18:09:42 +0530

    pattern 13

--- src/patternlabjava/Pattern13.java
+++ src/patternlabjava/Pattern13.java
@@ -6,16 +6,27 @@ public class Pattern13 {
     public static void main(String[] args) {
         int n = InputUtil.nextInt();
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                int count = 2;
+            for (int j = 1;j<=n;j++){
+                int start = (j - 1) * n + 1;
+                int val;
                 if (j % 2 != 0) {
-                    System.out.print(n * count + 2 + i  + " ");
+                    val = start + (i - 1);
                 } else {
-                    System.out.print("  *   ");
+                    val = start + (n - i);
                 }
+
+                System.out.print(val + " ");
             }
             System.out.println();
         }
     }
 }
 
+
+/*
+1 10 11 20 21
+2 9 12 19 22
+3 8 13 18 23
+4 7 14 17 24
+5 6 15 16 25
+*/
